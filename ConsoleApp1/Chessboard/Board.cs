9607d6df2e7a0894900a0cb6bdd9cdfbfefba2af
Alan Farias <alanfl885@gmail.com>
2025-12-05 01:17:44 -0300

    Tranto erro com exceção personalizada

--- ConsoleApp1/Chessboard/Board.cs
+++ ConsoleApp1/Chessboard/Board.cs
@@ -17,12 +17,41 @@ namespace Chessboard
         {
             return Pieces[Line, Column];
         }
+        public Piece piece(Position position)
+        {
+            return piece(position.Line, position.Column);
+        }
+        public bool PieceExists(Position pos)
+        {
+            ValidPosition(pos);
+            return piece(pos) != null;
+        }
 
         public void PutPiece(Piece p, Position pos)
         {
+            if (PieceExists(pos))
+            {
+                throw new BoardException("Já existe uma peça nessa posição!");
+            }
             Pieces[pos.Line, pos.Column] = p;
             p.position = pos;
         }
 
+        public bool ValidPosition(Position pos) 
+        { 
+            if (pos.Line<0 || pos.Line>=Lines || pos.Column<0 || pos.Column>=Columns)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public void ValidatePosition(Position pos)
+        {
+            if (!ValidPosition(pos))
+            {
+                throw new BoardException("Posição inválida");
+            }
+        }
     }
 }
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,14 +6,22 @@ class Program
 
     static void Main(string[] args)
     {
-        Board board = new Board(8, 8);
-        board.PutPiece(new Tower(Color.Black, board), new Position(0, 0));
-        board.PutPiece(new Tower(Color.Black, board), new Position(1, 3));
-        board.PutPiece(new King(Color.Black, board), new Position(2, 4));
+        try
+        {
+            Board board = new Board(8, 8);
+            board.PutPiece(new Tower(Color.Black, board), new Position(0, 0));
+            board.PutPiece(new Tower(Color.Black, board), new Position(1, 3));
+            board.PutPiece(new King(Color.Black, board), new Position(0, 4));
 
-        Screen.PrintBoard(board);
+            Screen.PrintBoard(board);
 
-        Console.ReadLine();
+            Console.ReadLine();
+
+        }
+        catch (BoardException e)
+        {
+            Console.WriteLine(e.Message);
+        }
     }
 }
 
